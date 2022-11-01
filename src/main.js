dafcfb39dbf6004adbca988ac0263036a97f5723
fmhdnr <939348417@qq.com>
2022-11-01 18:38:55 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui';	// Element 1
+import 'element-ui/lib/theme-chalk/index.css'; // Element 2
+
+import VueRouter from 'vue-router' // 路由 1
+import router from './router' // 路由 2
+import axios from "axios"
+import VueAxios from 'vue-axios'
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI); // Element 3
+Vue.use(VueRouter) // 路由 3
+Vue.use(VueAxios,axios)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer: {
+    // 代理服务器可以将路由中的指定前缀转发到指定的后端服务器中
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        // ws: true, // 是否启用websockets
+        changeOrigin: true,  // 代理时是否更改host
+        pathRewrite: {
+          '^/api': '' //这里理解成用'/api'代替target里面的地址
+        }
+      }
+    }
+  }
 })
